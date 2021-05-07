b28d0464b5acee0dba7f8c0a16db59cc81032e2d
TomKloucek <tktomasek@gmail.com>
2021-05-08 01:31:14 +0200

    Calendar done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import vueTextArea from 'vue-textarea-autosize';
+import 'vuetify/dist/vuetify.min.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(vuetify);
+Vue.use(vueTextArea);
+
+firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
